e18ea591d25bfe4f3790020f43635d46abab266d
ktdrzd <137915871+ktdrzd@users.noreply.github.com>
2024-07-12 20:04:35 +0300

    fix

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,6 +1,6 @@
 // import css from "./Header.module";
 import css from "./Header.module.css";
-import { BsMenuButtonWideFill } from "react-icons/bs";
+// import { BsMenuButtonWideFill } from "react-icons/bs";
 
 const Header = () => {
   return (
@@ -9,9 +9,16 @@ const Header = () => {
         Drozd Kateryna <br />
         Fullstack Developer
       </h2>
-      <button className={css.button}>
+      <div className={css.menu}>
+        <p>Головна</p>
+        <p>Про мене</p>
+        <p>Навички</p>
+        <p>Проєкти</p>
+        <p>Контакти</p>
+      </div>
+      {/* <button className={css.button}>
       <BsMenuButtonWideFill className={css.burger}/>
-      </button>
+      </button> */}
     </div>
   );
 };
